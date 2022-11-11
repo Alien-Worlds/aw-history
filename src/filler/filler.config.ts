5c6200593cffffdd1b6ac7f0711861f7cbb51298
rkamysz <radoslaw.kamysz@gmail.com>
2022-11-11 17:38:10 +0100

    optional broadcast config

--- src/filler/filler.config.ts
+++ src/filler/filler.config.ts
@@ -1,16 +1,15 @@
+import { MongoConfig } from '@alien-worlds/api-core';
 import { BlockRangeScanConfig } from '../common/block-range-scanner';
+import { BroadcastConfig } from '../common/broadcast';
 
 export type FillerConfig = {
-  broadcast: {
-    url: string;
-    channel: string;
-  };
+  broadcast: BroadcastConfig;
   blockchain: {
     endpoint: string;
     chainId: string;
   };
   scanner: BlockRangeScanConfig;
-  mongo: { url: string; dbName: string };
+  mongo: MongoConfig;
   startBlock?: bigint;
   endBlock?: bigint;
   mode: string;
