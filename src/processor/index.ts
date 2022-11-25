0000550fce3ef17372a5d4bea1fb17f8afb84bdc
rkamysz <radoslaw.kamysz@gmail.com>
2022-11-25 17:34:57 +0100

    add set-abi to processor index

--- src/processor/index.ts
+++ src/processor/index.ts
@@ -2,9 +2,18 @@ export * from './broadcast/delta-processor.mapper';
 export * from './broadcast/delta-processor.message-content';
 export * from './broadcast/trace-processor.mapper';
 export * from './broadcast/trace-processor.message-content';
-export * from './tasks';
+export * from './broadcast/processor.broadcast';
+
+export * from './tasks/action-trace-processor.task';
+export * from './tasks/action-trace-processor.task-input';
+export * from './tasks/delta-processor.task';
+export * from './tasks/delta-processor.task-input';
+export * from './tasks/processor.task';
+
+export * from './tasks/setabi/set-abi.processor';
+export * from './tasks/setabi/set-abi-processor.task-input';
+export * from './tasks/setabi/set-abi.types';
 
 export * from './processor.config';
-export * from './broadcast/processor.broadcast';
 export * from './processor.types';
 export * from './start-processor';
