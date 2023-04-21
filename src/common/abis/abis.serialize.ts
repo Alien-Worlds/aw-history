9d9a1278bdf0f9387de6dcac49cc14e631b29306
rkamysz <radoslaw.kamysz@gmail.com>
2023-04-21 13:30:41 +0200

    drop node-abieos

--- src/common/abis/abis.serialize.ts
+++ src/common/abis/abis.serialize.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Serialize } from 'eosjs';
-import abieos from '@eosrio/node-abieos';
 import { Authorization, hexToUint8Array } from 'eosjs/dist/eosjs-serialize';
 import { Abi } from 'eosjs/dist/eosjs-rpc-interfaces';
 import { log } from '@alien-worlds/api-core';
@@ -31,47 +30,42 @@ export class AbisSerialize {
     hex: string
   ): T => {
     try {
-      if (process.platform === 'linux') {
-        abieos.load_abi_hex(account, hex);
-        const type = abieos.get_type_for_action(account, action);
-        const json = abieos.bin_to_json(account, type, Buffer.from(data));
-        return json as T;
-      } else {
-        const authorization: Authorization[] = [];
-        const textEncoder = new TextEncoder();
-        const textDecoder = new TextDecoder();
-        const bytes = hexToUint8Array(hex);
-        const abiTypes = Serialize.getTypesFromAbi(Serialize.createAbiTypes());
-        const buffer = new Serialize.SerialBuffer({
-          textEncoder,
-          textDecoder,
-          array: bytes,
-        });
-        buffer.restartRead();
-        const abi: Abi = abiTypes.get('abi_def').deserialize(buffer);
-        const types = Serialize.getTypesFromAbi(Serialize.createInitialTypes(), abi);
-        const actions = new Map();
-        for (const { name, type } of abi.actions) {
-          actions.set(name, Serialize.getType(types, type));
-        }
-        const contract = { types, actions };
-        const deserializedAction = Serialize.deserializeAction(
-          contract,
-          account,
-          action,
-          authorization,
-          data,
-          new TextEncoder(),
-          new TextDecoder()
-        );
-
-        if(!deserializedAction.data) {
-          log(`Serialized object is empty check the result of "Serialize.deserializeAction"`);
-          log(deserializedAction);
-        }
-
-        return deserializedAction.data as T;
+      const authorization: Authorization[] = [];
+      const textEncoder = new TextEncoder();
+      const textDecoder = new TextDecoder();
+      const bytes = hexToUint8Array(hex);
+      const abiTypes = Serialize.getTypesFromAbi(Serialize.createAbiTypes());
+      const buffer = new Serialize.SerialBuffer({
+        textEncoder,
+        textDecoder,
+        array: bytes,
+      });
+      buffer.restartRead();
+      const abi: Abi = abiTypes.get('abi_def').deserialize(buffer);
+      const types = Serialize.getTypesFromAbi(Serialize.createInitialTypes(), abi);
+      const actions = new Map();
+      for (const { name, type } of abi.actions) {
+        actions.set(name, Serialize.getType(types, type));
       }
+      const contract = { types, actions };
+      const deserializedAction = Serialize.deserializeAction(
+        contract,
+        account,
+        action,
+        authorization,
+        data,
+        new TextEncoder(),
+        new TextDecoder()
+      );
+
+      if (!deserializedAction.data) {
+        log(
+          `Serialized object is empty check the result of "Serialize.deserializeAction"`
+        );
+        log(deserializedAction);
+      }
+
+      return deserializedAction.data as T;
     } catch (error) {
       log(error);
       return null;
@@ -85,48 +79,41 @@ export class AbisSerialize {
     hex: string
   ): T => {
     try {
-      if (process.platform === 'linux') {
-        abieos.load_abi_hex(account, hex);
-        const type = abieos.get_type_for_table(account, table);
-        const json = abieos.bin_to_json(account, type, Buffer.from(data));
-        return json as T;
-      } else {
-        const textEncoder = new TextEncoder();
-        const textDecoder = new TextDecoder();
-        const bytes = hexToUint8Array(hex);
-        const abiTypes = Serialize.getTypesFromAbi(Serialize.createAbiTypes());
-        const buffer = new Serialize.SerialBuffer({
-          textEncoder,
-          textDecoder,
-          array: bytes,
-        });
-        buffer.restartRead();
-        const abi: Abi = abiTypes.get('abi_def').deserialize(buffer);
-        const types = Serialize.getTypesFromAbi(Serialize.createInitialTypes(), abi);
-        const actions = new Map();
-        for (const { name, type } of abi.actions) {
-          actions.set(name, Serialize.getType(types, type));
-        }
-        const contract = { types, actions };
-
-        let this_table: AbiTableJson, type: string;
-        for (const t of abi.tables) {
-          if (t.name === table) {
-            this_table = t;
-            break;
-          }
-        }
-
-        if (this_table) {
-          type = this_table.type;
-        } else {
-          return null;
-        }
-
-        const sb = new Serialize.SerialBuffer({ textEncoder, textDecoder, array: data });
-
-        return contract.types.get(type).deserialize(sb) as T;
+      const textEncoder = new TextEncoder();
+      const textDecoder = new TextDecoder();
+      const bytes = hexToUint8Array(hex);
+      const abiTypes = Serialize.getTypesFromAbi(Serialize.createAbiTypes());
+      const buffer = new Serialize.SerialBuffer({
+        textEncoder,
+        textDecoder,
+        array: bytes,
+      });
+      buffer.restartRead();
+      const abi: Abi = abiTypes.get('abi_def').deserialize(buffer);
+      const types = Serialize.getTypesFromAbi(Serialize.createInitialTypes(), abi);
+      const actions = new Map();
+      for (const { name, type } of abi.actions) {
+        actions.set(name, Serialize.getType(types, type));
       }
+      const contract = { types, actions };
+
+      let this_table: AbiTableJson, type: string;
+      for (const t of abi.tables) {
+        if (t.name === table) {
+          this_table = t;
+          break;
+        }
+      }
+
+      if (this_table) {
+        type = this_table.type;
+      } else {
+        return null;
+      }
+
+      const sb = new Serialize.SerialBuffer({ textEncoder, textDecoder, array: data });
+
+      return contract.types.get(type).deserialize(sb) as T;
     } catch (e) {
       return null;
     }
