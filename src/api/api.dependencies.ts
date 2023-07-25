a3a0274d3abed754ff6ab7c2270318717f74cb12
rkamysz <radoslaw.kamysz@gmail.com>
2023-07-25 13:23:14 +0200

    update deps

--- src/api/api.dependencies.ts
+++ src/api/api.dependencies.ts
@@ -1,4 +1,4 @@
-import { Result } from '@alien-worlds/api-core';
+import { Container, Result, Route, UnknownObject } from '@alien-worlds/api-core';
 import { DatabaseConfigBuilder, Dependencies } from '../common';
 import { Api } from './api';
 
@@ -8,7 +8,11 @@ import { Api } from './api';
  */
 export abstract class ApiDependencies extends Dependencies {
   public api: Api;
+  public ioc: Container;
   public databaseConfigBuilder: DatabaseConfigBuilder;
 
-  public abstract initialize(): Promise<Result>;
+  public abstract initialize(
+    setupIoc: (config: UnknownObject, container: Container) => Promise<void>,
+    routesProvider: (container: Container) => Route[]
+  ): Promise<Result>;
 }
